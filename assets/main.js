cc652beadf9a5851e9ade5acd1b665c65012256c
Catraco <93885462+catraco@users.noreply.github.com>
2022-12-21 00:14:13 +0100

    Upload 84752

--- assets/main.js
+++ assets/main.js
@@ -1,36 +1,27 @@
+"use strict";
+
+var choices = document.getElementsByClassName("choices")[0];
+var options = document.querySelectorAll(".options");
 
 // Scroll header function //
 function scrollHeader(){
 
-    if(
-        document.getElementsByClassName("choices")[0].style.display == "none" ||
-        document.getElementsByClassName("choices")[0].style.display == ""
-    ){
-        document.getElementsByClassName("choices")[0].style.display = "block";
-        if(document.getElementsByClassName("choices")[0].classList.contains("choices-animation-close-class")){
-            document.getElementsByClassName("choices")[0].classList.remove("choices-animation-close-class");
+    if( choices.style.display == "none" || choices.style.display == "" ){
+        choices.style.display = "block";
+        if( choices.classList.contains("choices-animation-close-class") ){
+            choices.classList.remove("choices-animation-close-class");
         }
-        document.getElementsByClassName("choices")[0].classList.add("choices-animation-open-class");
+        choices.classList.add("choices-animation-open-class");
     }else{
-        document.getElementsByClassName("choices")[0].classList.remove("choices-animation-open-class");
-        document.getElementsByClassName("choices")[0].classList.add("choices-animation-close-class");
-        setTimeout(()=>{
-            document.getElementsByClassName("choices")[0].style.display = "none";
-        }, 400);
+        choices.classList.remove("choices-animation-open-class");
+        choices.classList.add("choices-animation-close-class");
+        setTimeout(()=>{ choices.style.display = "none"; }, 400);
     }
 
 }
 
-// Goto function //
-function gotoSection(id){
-
-    scrollHeader();
-    document.getElementById(id).click();
-    
-}
-
-// On window load //
-window.onload = () => {
+// Setup function //
+function setup(){
 
     setTimeout(()=>{
         document.getElementById("loading").style.display = "none";
@@ -38,9 +29,21 @@ window.onload = () => {
     }, 500);
 
     document.getElementsByClassName("three-bars")[0].onclick = () => {
-
         scrollHeader();
-
     }
+
+    options.forEach( (option) => {
+        option.addEventListener("click", ()=>{
+            scrollHeader();
+            document.getElementById(option.getAttribute("section")).click();
+        });
+    });
+
+}
+
+// On window load //
+window.onload = () => {
+
+    setup();
 
 }
